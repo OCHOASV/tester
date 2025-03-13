f14e6eb60b725fed1946370828c845c961487fbf
Carlos Ochoa <34662161+OCHOASV@users.noreply.github.com>
2025-03-13 18:36:53 +0100

    Delete trips directory

--- trips/trips2/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include 'config.php';
-    $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
-
-    if ($connection->connect_error) {
-        $connection->connect_error;
-    }
-?>
